696f128384f3c25a2db41dc0323293d6996f719c
John Elizarraras <elizarrj27@gmail.com>
2023-10-17 15:11:59 -0500

    use correct fdr ordering

--- webgestalt_lib/src/stat.rs
+++ webgestalt_lib/src/stat.rs
@@ -15,20 +15,18 @@ pub fn adjust(p_vals: &[f64]) -> Vec<f64> {
     carriers.sort_by(|a, b| a.p.partial_cmp(&b.p).unwrap());
     let m = carriers.len();
     let mut fdr_vals = vec![0.0; m];
-    for (i, carrier) in carriers.iter().enumerate() {
+    let mut prev_fdr = 1.0;
+    for (i, carrier) in carriers.iter().enumerate().rev() {
         let mut fdr = carrier.p * m as f64 / (i + 1) as f64;
         if fdr > 1.0 {
             fdr = 1.0;
         }
-        fdr_vals[carrier.original_order] = fdr;
-    }
-    let mut prev_fdr = fdr_vals[0];
-    for i in 1..fdr_vals.len() {
-        if prev_fdr > fdr_vals[i] {
-            fdr_vals[i] = prev_fdr;
+        if fdr > prev_fdr {
+            fdr = prev_fdr;
         } else {
-            prev_fdr = fdr_vals[i];
+            prev_fdr = fdr;
         }
+        fdr_vals[carrier.original_order] = fdr;
     }
     fdr_vals
 }
