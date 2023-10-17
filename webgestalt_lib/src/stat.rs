56da7407e625867c3aa0d98a97bfbcdf30c7033d
John Elizarraras <elizarrj27@gmail.com>
2023-10-17 13:02:21 -0500

    potential fix for fdr

--- webgestalt_lib/src/stat.rs
+++ webgestalt_lib/src/stat.rs
@@ -12,16 +12,16 @@ pub fn adjust(p_vals: &Vec<f64>) -> Vec<f64> {
             original_order: i,
         })
         .collect();
-    carriers.sort_by(|a, b| b.p.partial_cmp(&a.p).unwrap());
+    carriers.sort_by(|a, b| a.p.partial_cmp(&b.p).unwrap());
     let m = carriers.len();
     let mut fdr_vals = vec![0.0; m];
-    let mut prev_fdr = 1.0;
+    let mut prev_fdr = 0.0;
     for (i, carrier) in carriers.iter().enumerate() {
         let mut fdr = carrier.p * m as f64 / (i + 1) as f64;
         if fdr > 1.0 {
             fdr = 1.0;
         }
-        if fdr > prev_fdr {
+        if fdr < prev_fdr {
             fdr_vals[carrier.original_order] = prev_fdr;
         } else {
             fdr_vals[carrier.original_order] = fdr;
